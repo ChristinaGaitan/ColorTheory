4a86e8c5bf0e6e320ce8f7005b122c06e2f19789
ChristinaGaitan <christina.gaitan@gmail.com>
2021-03-19 15:02:48 -0600

    First files version

---
+++ main.js
@@ -0,0 +1,12 @@
+const productImage = document.querySelector(".card__img");
+
+// replace the emoji with an svg version that will scale to larger sizes
+twemoji.parse(productImage, {
+  folder: "svg",
+  ext: ".svg",
+  callback: function (icon, options, variant) {
+    productImage.style.display = "block";
+
+    return "".concat(options.base, options.size, "/", icon, options.ext);
+  }
+});
