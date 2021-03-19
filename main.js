877552798f4a5a112e011c61f0baa6a4a53f92ae
ChristinaGaitan <christina.gaitan@gmail.com>
2021-03-19 15:36:37 -0600

    Add twemoji library

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const productImage = document.querySelector(".card__img");
+window.onload = function() {
+  const productImage = document.querySelector(".card__img");
 
-// replace the emoji with an svg version that will scale to larger sizes
-twemoji.parse(productImage, {
-  folder: "svg",
-  ext: ".svg",
-  callback: function (icon, options, variant) {
-    productImage.style.display = "block";
+  // replace the emoji with an svg version that will scale to larger sizes
+  twemoji.parse(productImage, {
+    folder: "svg",
+    ext: ".svg",
+    callback: function (icon, options, variant) {
+      productImage.style.display = "block";
 
-    return "".concat(options.base, options.size, "/", icon, options.ext);
-  }
-});
+      return "".concat(options.base, options.size, "/", icon, options.ext);
+    }
+  });
+};
